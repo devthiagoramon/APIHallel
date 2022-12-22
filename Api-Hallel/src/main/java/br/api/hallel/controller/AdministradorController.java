918803c45e0b41906fb9e624ec2cb4b148788ece
devthiagoramon <dev.thiagoramon@gmail.com>
2022-12-22 00:08:14 -0400

    CRUD administador, acesso administrador e alteração listagem membro

---
+++ Api-Hallel/src/main/java/br/api/hallel/controller/AdministradorController.java
@@ -0,0 +1,55 @@
+package br.api.hallel.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.api.hallel.model.Administrador;
+import br.api.hallel.service.AdministradorService;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/administrador")
+public class AdministradorController {
+
+    @Autowired
+    private AdministradorService service;
+
+    @PostMapping("/create")
+    public String inserirAdministrador(@RequestBody Administrador administrador) {
+        return this.service.inserirAdministrador(administrador);
+    }
+
+    @GetMapping("")
+    public List<Administrador> listarAdministradores() {
+        return this.service.listarTodosAdministradores();
+    }
+
+    @GetMapping("/{id}")
+    public Administrador listarAdministradorPorId(@PathVariable String id) {
+        return this.service.findAdministrador(id);
+    }
+
+    @GetMapping("/{id}/{senhaAcesso}}")
+    public Administrador acessarAdministrador(@PathVariable(value = "id") String id,
+            @PathVariable(value = "senhaAcesso") String senhaAcesso) {
+        return this.service.acessarAdministrador(id, senhaAcesso);
+    }
+
+    @PostMapping("/{id}/update")
+    public String alterarAdministrador(@PathVariable(value = "id") String id, @RequestBody Administrador administradorNovo){
+        return this.service.alterarAdministrador(id, administradorNovo);
+    }
+
+    @PostMapping("/{id}/delete")
+    public String deletarAdministrador(@PathVariable String id){
+        return this.service.deletarAdministrador(id);
+    }
+}
--- Api-Hallel/src/main/java/br/api/hallel/service/interfaces/AdministradorInterface.java
+++ Api-Hallel/src/main/java/br/api/hallel/service/interfaces/AdministradorInterface.java
@@ -1,5 +1,15 @@
 package br.api.hallel.service.interfaces;
 
+import java.util.List;
+
+import br.api.hallel.model.Administrador;
+
 public interface AdministradorInterface {
     
+    public String inserirAdministrador(Administrador administrador);
+    public List<Administrador> listarTodosAdministradores();
+    public Administrador findAdministrador();
+    public Administrador acessarAdministrador(String id, String senhaAcesso);
+    public String deletarAdministrador(String id);
+
 }
