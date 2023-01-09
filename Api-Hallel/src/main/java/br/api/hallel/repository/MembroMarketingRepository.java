f42fbb2a11d5054aa7fe84ddee207e4d850f533d
MIGUEL-ARCANJO <2021333513@ifam.edu.br>
2023-01-09 11:24:10 -0400

    CRUD de membrosMarketing(agora foi), algumas modificações no Controller de Eventos e MembrosMarketing

--- Api-Hallel/src/main/java/br/api/hallel/repository/MembroMarketingRepository.java
+++ Api-Hallel/src/main/java/br/api/hallel/repository/MembroMarketingRepository.java
@@ -1,9 +1,15 @@
 package br.api.hallel.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import br.api.hallel.model.MembroMarketing;
+
 @Repository
-public interface MembroMarketingRepository extends MongoRepository<MembroMarketingRepository, String>{
-    
+public interface MembroMarketingRepository extends MongoRepository<MembroMarketing, String>{
+
+    public Optional<MembroMarketing> findByEmail(String email);
+
 }
--- Api-Hallel/src/main/java/br/api/hallel/service/interfaces/MembroMarketingInterface.java
+++ Api-Hallel/src/main/java/br/api/hallel/service/interfaces/MembroMarketingInterface.java
@@ -1,5 +1,23 @@
 package br.api.hallel.service.interfaces;
 
+import java.util.List;
+
+import br.api.hallel.model.MembroMarketing;
+
 public interface MembroMarketingInterface {
-    
+
+    public MembroMarketing createMembroMarketing(MembroMarketing membro);
+
+    public List<MembroMarketing> listAllMembrosMarketing();
+
+    public MembroMarketing findMembroId(String id);
+
+    public MembroMarketing updateById(String id);
+
+    public MembroMarketing updateByEmail(String email);
+
+    public void deleteById(String id);
+
+    public MembroMarketing findByEmail(String email);
+
 }
