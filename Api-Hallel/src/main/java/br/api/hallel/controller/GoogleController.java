39482cc6f188f032b2729fc8fafaf8d8031f1fe9
MIGUEL-ARCANJO <2021333513@ifam.edu.br>
2023-01-27 12:54:15 -0400

    Ajustando Login e Cadastro usando Oauth2(Google)

--- Api-Hallel/src/main/java/br/api/hallel/controller/GoogleController.java
+++ Api-Hallel/src/main/java/br/api/hallel/controller/GoogleController.java
@@ -1,31 +1,45 @@
 package br.api.hallel.controller;
 
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
+import br.api.hallel.payload.requerimento.LoginRequerimento;
+import br.api.hallel.payload.requerimento.SolicitarCadastroRequerimento;
+import br.api.hallel.payload.resposta.AuthenticationResponse;
+import br.api.hallel.service.MainService;
+import br.api.hallel.service.MembroService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/google")
 public class GoogleController {
 
+    @Autowired
+    private MembroService service;
+    @Autowired
+    private MainService mainService;
 
-    @PostMapping("/login")
-    public void loginGoogle(){
+    @GetMapping("/cadastro")
+    public AuthenticationResponse cadastroGoogle(@Valid @RequestBody SolicitarCadastroRequerimento solicitarCadastroRequerimento, OAuth2AuthenticationToken token) {
+        solicitarCadastroRequerimento.setNome(token.getPrincipal().getAttribute("name"));
+        solicitarCadastroRequerimento.setEmail(token.getPrincipal().getAttribute("email"));
 
+        return this.mainService.solicitarCadastro(solicitarCadastroRequerimento);
+    }
+
+    @GetMapping("/login")
+    public AuthenticationResponse loginGoogle(@Valid @RequestBody LoginRequerimento loginRequerimento){
+        return this.mainService.logar(loginRequerimento);
     }
 
     @PostMapping("/logout")
-    public void logoutGoogle(){
+    public void logoutGoogle() {
+
     }
 
     @PostMapping("/callback/{registrationId}")
-    public void callbackGoogle(){
+    public void callbackGoogle() {
 
     }
+
 }
