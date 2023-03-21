c4f61584937732ac2520cde7b3989634e72a8662
devthiagoramon <dev.thiagoramon@gmail.com>
2023-03-21 09:06:55 -0300

    Listando Doações

--- Api-Hallel/src/main/java/br/api/hallel/service/interfaces/DoacaoInterface.java
+++ Api-Hallel/src/main/java/br/api/hallel/service/interfaces/DoacaoInterface.java
@@ -3,8 +3,12 @@ package br.api.hallel.service.interfaces;
 import br.api.hallel.model.Doacao;
 import br.api.hallel.payload.requerimento.DoacaoReq;
 
+import java.util.List;
+
 public interface DoacaoInterface {
 
     Doacao doar(DoacaoReq doacaoReq);
+    List<Doacao> listAllDoacoes();
+    Doacao listDoacaoById(String id);
 
 }
