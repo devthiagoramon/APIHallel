67916a98bb29a0117d330a0a805b19cec398071d
devthiagoramon <dev.thiagoramon@gmail.com>
2023-03-24 11:23:24 -0300

    Ajuste pre reuniao, pagamento associado

--- Api-Hallel/src/main/java/br/api/hallel/payload/resposta/AssociadoPagamentosRes.java
+++ Api-Hallel/src/main/java/br/api/hallel/payload/resposta/AssociadoPagamentosRes.java
@@ -1,5 +1,6 @@
 package br.api.hallel.payload.resposta;
 
+import br.api.hallel.model.AssociadoRole;
 import br.api.hallel.model.Transacao;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -11,6 +12,8 @@ import lombok.NoArgsConstructor;
 public class AssociadoPagamentosRes {
     private String nome;
     private String email;
+    private Boolean isPago;
+    private AssociadoRole situacao;
     private Transacao transacao;
 
 }
