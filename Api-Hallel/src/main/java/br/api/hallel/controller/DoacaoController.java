5bc2ab36adad86277745b6b075b7cb4bf52a90c1
devthiagoramon <dev.thiagoramon@gmail.com>
2023-03-28 14:47:03 -0400

    Doações por objeto completo

--- Api-Hallel/src/main/java/br/api/hallel/controller/DoacaoController.java
+++ Api-Hallel/src/main/java/br/api/hallel/controller/DoacaoController.java
@@ -1,8 +1,11 @@
 package br.api.hallel.controller;
 
 import br.api.hallel.model.Doacao;
+import br.api.hallel.model.DoacaoObjeto;
+import br.api.hallel.payload.requerimento.DoacaoObjetoReq;
 import br.api.hallel.payload.requerimento.DoacaoReq;
 import br.api.hallel.payload.resposta.DoacoesDinheiroListaAdmResponse;
+import br.api.hallel.payload.resposta.DoacoesObjetoListaAdmResponse;
 import br.api.hallel.service.DoacaoService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -21,12 +24,30 @@ public class DoacaoController {
     private DoacaoService service;
 
     @PostMapping("/doar")
-    private Doacao doar(DoacaoReq doacaoReq){
+    private Doacao doar(@RequestBody DoacaoReq doacaoReq){
         return this.service.doar(doacaoReq);
+    }
+
+    @PostMapping("/doarObjeto")
+    private DoacaoObjeto doarObjeto(@RequestBody DoacaoObjetoReq doacaoObjetoReq){
+        return this.service.doarObjeto(doacaoObjetoReq);
+    }
+
+    @PostMapping("{id}/recebido")
+    public DoacaoObjeto objetoRecebido(@PathVariable String id){
+        return this.service.objetoRecebido(id);
+    }
+
+    @PostMapping("{id}/naoRecebido")
+    public DoacaoObjeto objetoNaoRecebido(@PathVariable String id){
+        return this.service.objetoNaoRecebido(id);
     }
 
     @GetMapping("/list")
     private List<DoacoesDinheiroListaAdmResponse> doacaoList() {return this.service.listAllDoacoes();}
+
+    @GetMapping("/listObjetos")
+    private List<DoacoesObjetoListaAdmResponse> doacoesObjList(){return this.service.listAllDoacoesObjeto();};
 
     @GetMapping("/{id}")
     private Doacao listDoacaoById(@PathVariable String id){return this.service.listDoacaoById(id);}
--- Api-Hallel/src/main/java/br/api/hallel/service/interfaces/DoacaoInterface.java
+++ Api-Hallel/src/main/java/br/api/hallel/service/interfaces/DoacaoInterface.java
@@ -1,8 +1,11 @@
 package br.api.hallel.service.interfaces;
 
 import br.api.hallel.model.Doacao;
+import br.api.hallel.model.DoacaoObjeto;
+import br.api.hallel.payload.requerimento.DoacaoObjetoReq;
 import br.api.hallel.payload.requerimento.DoacaoReq;
 import br.api.hallel.payload.resposta.DoacoesDinheiroListaAdmResponse;
+import br.api.hallel.payload.resposta.DoacoesObjetoListaAdmResponse;
 
 import java.util.List;
 
@@ -12,4 +15,11 @@ public interface DoacaoInterface {
     List<DoacoesDinheiroListaAdmResponse> listAllDoacoes();
     Doacao listDoacaoById(String id);
 
+    List<DoacoesObjetoListaAdmResponse> listAllDoacoesObjeto();
+
+    DoacaoObjeto doarObjeto(DoacaoObjetoReq doacaoObjeto);
+
+    DoacaoObjeto objetoRecebido(String id);
+
+    DoacaoObjeto objetoNaoRecebido(String id);
 }
