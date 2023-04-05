1a3345aef07b6e76bd38756127f371d1653c1c96
MIGUEL-ARCANJO <2021333513@ifam.edu.br>
2023-04-05 19:05:49 -0400

    CRUD de Cursos

---
+++ Api-Hallel/src/main/java/br/api/hallel/service/CursoService.java
@@ -0,0 +1,51 @@
+package br.api.hallel.service;
+
+import br.api.hallel.model.Curso;
+import br.api.hallel.repository.CursoRepository;
+import br.api.hallel.service.interfaces.CursoInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CursoService implements CursoInterface {
+
+    @Autowired
+    CursoRepository repository;
+
+    @Override
+    public Curso createCurso(Curso curso) {
+        return this.repository.insert(curso);
+    }
+
+    @Override
+    public List<Curso> listAllCursos() {
+        return this.repository.findAll();
+    }
+
+    @Override
+    public Curso listCursoById(String id) {
+        return this.repository.findById(id).orElseThrow(() ->
+                new RuntimeException("Curso com id " + id + " não existe!"));
+    }
+
+    @Override
+    public Curso updateCurso(Curso curso) {
+
+        return this.repository.save(curso);
+    }
+
+    @Override
+    public void deleteCurso(String id) {
+        Optional<Curso> optional = this.repository.findById(id);
+
+        if(optional.isPresent()){
+            this.repository.deleteById(id);
+        }else{
+            throw new RuntimeException("Curso não criado!");
+        }
+
+    }
+}
