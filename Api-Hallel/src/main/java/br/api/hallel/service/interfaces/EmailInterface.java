94af2228f4656558202ad7ea01a792ce6722538c
MIGUEL-ARCANJO <2021333513@ifam.edu.br>
2023-04-24 20:46:12 -0400

    Adicionando mensagens de email para aniversariantes do dia

--- Api-Hallel/src/main/java/br/api/hallel/service/interfaces/EmailInterface.java
+++ Api-Hallel/src/main/java/br/api/hallel/service/interfaces/EmailInterface.java
@@ -1,12 +1,10 @@
 package br.api.hallel.service.interfaces;
 
-import br.api.hallel.model.Email;
 import br.api.hallel.payload.requerimento.EmailRequest;
 import jakarta.mail.MessagingException;
 
 public interface EmailInterface {
 
-    Email sendMail(EmailRequest response);
-    Email sendMailAttachment(EmailRequest response) throws MessagingException;
-
+    void sendMail(EmailRequest response);
+    void sendMailAttachment(EmailRequest response) throws MessagingException;
 }
